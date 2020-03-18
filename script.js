07c8ddd89023656fe54e230771929f6fc4e5ada7
Ken Shiraishi <kshiraishi22@gmail.com>
2020-03-18 15:45:55 -0400

    added character variables

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+// Variables
+var lowerChar = "abcdefghijklmnopqrstuvwxyz"
+var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var num = "0123456789";
+var specialChar = "!#$%&'()*+,-./:;<=>?@^`|~";
+
+// Calling All Elements from DOM
+var 
+
+// Generate a random number between 0 and 9
+  // var randNum = Math.floor(Math.random() * 10) + 1;
+  // console.log(randNum)
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lengthInput = document.querySelector("#length");
+
+generateBtn.addEventListener("click", function() {
+  if (lengthInput < 8) {
+    // alert("The password will be too short. Try again.")
+  };
+  
+  if (lengthInput > 128) {
+    // alert("The password will be too long. Try again.")
+  };
+});
 
 // Write password to the #password input
 function writePassword() {
@@ -11,4 +35,5 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword) 
+
