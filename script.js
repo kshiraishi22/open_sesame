a78580f0c1618a1264554bea05f60b79ed68f6c8
Ken Shiraishi <kshiraishi22@gmail.com>
2020-03-27 17:38:34 -0400

    fully functional

--- script.js
+++ script.js
@@ -1,39 +1,74 @@
 // Variables
 var lowerChar = "abcdefghijklmnopqrstuvwxyz"
 var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var num = "0123456789";
+var charNum = "0123456789";
 var specialChar = "!#$%&'()*+,-./:;<=>?@^`|~";
 
-// Calling All Elements from DOM
-var 
+// Calling All Needed Elements from DOM
+// var lengthEl = document.getElementById("length");
+var lowerCharEl = document.getElementById("lowerCase");
+var upperCharEl = document.getElementById("upperCase");
+var numbersEl = document.getElementById("numbers");
+var specialCharEl = document.getElementById("specialCharacters");
+var passwordEl = document.getElementById("password");
+var generateBtn = document.getElementById("generate");
+var slider = document.getElementById("slider");
+var passLength = document.getElementById("passLength");
 
-// Generate a random number between 0 and 9
-  // var randNum = Math.floor(Math.random() * 10) + 1;
-  // console.log(randNum)
+// Syncing Password Length Input and Slider
+slider.addEventListener("input", syncInputSlider);
+passLength.addEventListener("input", syncInputSlider);
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-var lengthInput = document.querySelector("#length");
-
-generateBtn.addEventListener("click", function() {
-  if (lengthInput < 8) {
-    // alert("The password will be too short. Try again.")
-  };
-  
-  if (lengthInput > 128) {
-    // alert("The password will be too long. Try again.")
-  };
-});
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+function syncInputSlider (event) {
+  var value = event.target.value
+  slider.value = value
+  passLength.value = value
 }
 
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword) 
+var handleSubmit = function(event) {
+  event.preventDefault();
+  var charInventory = "";
+  var numOfChars = passLength.value;
+
+  if(lowerCharEl.checked === true) {
+    charInventory += lowerChar;
+    // console.log(charInventory);
+  }
+
+  if(upperCharEl.checked === true) {
+    charInventory += upperChar;
+    // console.log(charInventory);
+  }
+
+  if(numbersEl.checked === true) {
+    charInventory += charNum;
+    // console.log(charInventory);
+  }
+
+  if(specialCharEl.checked === true) {
+    charInventory += specialChar;
+    // console.log(charInventory);
+  }
+
+  var newPass = generatePassword(numOfChars, charInventory);
+  // console.log(newPass);
+
+  // Adding generated password into DOM
+  passwordEl.value = newPass;
+  passwordEl.setAttribute("value", newPass)
+}
+
+generateBtn.addEventListener("click", handleSubmit);
+
+function generatePassword(length, charInventory) {
+  var newPassword = "";
+
+  for(var i = 0; i < length; i++){
+  var randomNum = Math.floor(Math.random() * charInventory.length);
+  // console.log(randomNum)
+  newPassword += charInventory.charAt(randomNum)
+}
+return newPassword;
+}
+
 
